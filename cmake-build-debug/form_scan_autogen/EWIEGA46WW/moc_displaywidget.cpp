10943feb5761cb9e9db573b3aede2ecf48b7fe6b
William Westwood <wwstwd@gmail.com>
2024-06-07 09:23:08 +0100

    TODO: Add Database Use OCR fully

--- cmake-build-debug/form_scan_autogen/EWIEGA46WW/moc_displaywidget.cpp
+++ cmake-build-debug/form_scan_autogen/EWIEGA46WW/moc_displaywidget.cpp
@@ -37,10 +37,7 @@ struct qt_meta_stringdata_CLASSDisplayWidgetENDCLASS_t {};
 constexpr auto qt_meta_stringdata_CLASSDisplayWidgetENDCLASS = QtMocHelpers::stringData(
     "DisplayWidget",
     "updateFrame",
-    "",
-    "showSvg",
-    "std::string",
-    "svg"
+    ""
 );
 #else  // !QT_MOC_HAS_STRINGDATA
 #error "qtmochelpers.h not found or too old."
@@ -53,7 +50,7 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSDisplayWidgetENDCLASS[] = {
       12,       // revision
        0,       // classname
        0,    0, // classinfo
-       2,   14, // methods
+       1,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -61,12 +58,10 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSDisplayWidgetENDCLASS[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags, initial metatype offsets
-       1,    0,   26,    2, 0x08,    1 /* Private */,
-       3,    1,   27,    2, 0x108,    2 /* Private | MethodIsConst  */,
+       1,    0,   20,    2, 0x08,    1 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
-    QMetaType::Void, 0x80000000 | 4,    5,
 
        0        // eod
 };
@@ -81,10 +76,7 @@ Q_CONSTINIT const QMetaObject DisplayWidget::staticMetaObject = { {
         // Q_OBJECT / Q_GADGET
         QtPrivate::TypeAndForceComplete<DisplayWidget, std::true_type>,
         // method 'updateFrame'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        // method 'showSvg'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        QtPrivate::TypeAndForceComplete<const std::string &, std::false_type>
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
     >,
     nullptr
 } };
@@ -96,10 +88,10 @@ void DisplayWidget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _i
         (void)_t;
         switch (_id) {
         case 0: _t->updateFrame(); break;
-        case 1: _t->showSvg((*reinterpret_cast< std::add_pointer_t<std::string>>(_a[1]))); break;
         default: ;
         }
     }
+    (void)_a;
 }
 
 const QMetaObject *DisplayWidget::metaObject() const
@@ -121,13 +113,13 @@ int DisplayWidget::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 2)
+        if (_id < 1)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 2;
+        _id -= 1;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 2)
+        if (_id < 1)
             *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
-        _id -= 2;
+        _id -= 1;
     }
     return _id;
 }
--- cmake-build-debug/form_scan_autogen/include/ui_displaywidget.h
+++ cmake-build-debug/form_scan_autogen/include/ui_displaywidget.h
@@ -12,6 +12,7 @@
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QLabel>
+#include <QtWidgets/QProgressBar>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -24,9 +25,17 @@ public:
     QLabel *frameTitle;
     QLabel *frameLabel;
     QLabel *frameWidget;
+    QLabel *faceTitle;
     QLabel *faceLabel;
     QLabel *faceWidget;
+    QLabel *statsTitle;
     QLabel *statsLabel;
+    QLabel *cpuLabel;
+    QProgressBar *cpuBar;
+    QLabel *memLabel;
+    QProgressBar *memBar;
+    QLabel *uptimeLabel;
+    QLabel *resultsTitle;
     QLabel *resultsLabel;
     QLabel *svgWidget;
 
@@ -44,6 +53,20 @@ public:
 "    color: rgb(255, 255, 255);\n"
 "    font: 75 12pt \"Roboto Mono\";\n"
 "    }\n"
+"\n"
+"\n"
+"    QProgressBar {\n"
+"    border: none;\n"
+"    border-radius: 0px;\n"
+"    background-color: rgb(14, 14, 14);\n"
+"    }\n"
+"\n"
+"    QProgressBar::chunk {\n"
+"    background-color: #FFFFFF;\n"
+"    width: 20px;\n"
+"    margin: 2px;\n"
+"    }\n"
+"\n"
 "   "));
         logoLabel = new QLabel(DisplayWidget);
         logoLabel->setObjectName("logoLabel");
@@ -53,6 +76,11 @@ public:
         progressLabel->setObjectName("progressLabel");
         progressLabel->setGeometry(QRect(367, 20, 1533, 89));
         progressLabel->setFrameShape(QFrame::Shape::Box);
+        progressLabel->setStyleSheet(QString::fromUtf8("\n"
+"     font-weight: 500;\n"
+"     text-transform: uppercase;\n"
+"     font-size: 23pt;\n"
+"    "));
         frameTitle = new QLabel(DisplayWidget);
         frameTitle->setObjectName("frameTitle");
         frameTitle->setGeometry(QRect(20, 130, 419, 36));
@@ -60,6 +88,8 @@ public:
         frameTitle->setStyleSheet(QString::fromUtf8("\n"
 "     background-color: rgb(255, 255, 255);\n"
 "     color: rgb(0, 0, 0);\n"
+"     font-weight: 600;\n"
+"     text-transform: uppercase;\n"
 "    "));
         frameLabel = new QLabel(DisplayWidget);
         frameLabel->setObjectName("frameLabel");
@@ -68,6 +98,16 @@ public:
         frameWidget = new QLabel(frameLabel);
         frameWidget->setObjectName("frameWidget");
         frameWidget->setGeometry(QRect(5, 5, 623, 871));
+        faceTitle = new QLabel(DisplayWidget);
+        faceTitle->setObjectName("faceTitle");
+        faceTitle->setGeometry(QRect(703, 128, 419, 36));
+        faceTitle->setFrameShape(QFrame::Shape::Box);
+        faceTitle->setStyleSheet(QString::fromUtf8("\n"
+"     background-color: rgb(255, 255, 255);\n"
+"     color: rgb(0, 0, 0);\n"
+"     font-weight: 600;\n"
+"     text-transform: uppercase;\n"
+"    "));
         faceLabel = new QLabel(DisplayWidget);
         faceLabel->setObjectName("faceLabel");
         faceLabel->setGeometry(QRect(703, 179, 514, 514));
@@ -75,10 +115,63 @@ public:
         faceWidget = new QLabel(faceLabel);
         faceWidget->setObjectName("faceWidget");
         faceWidget->setGeometry(QRect(5, 5, 504, 504));
+        statsTitle = new QLabel(DisplayWidget);
+        statsTitle->setObjectName("statsTitle");
+        statsTitle->setGeometry(QRect(703, 710, 419, 36));
+        statsTitle->setFrameShape(QFrame::Shape::Box);
+        statsTitle->setStyleSheet(QString::fromUtf8("\n"
+"     background-color: rgb(255, 255, 255);\n"
+"     color: rgb(0, 0, 0);\n"
+"     font-weight: 600;\n"
+"     text-transform: uppercase;\n"
+"    "));
         statsLabel = new QLabel(DisplayWidget);
         statsLabel->setObjectName("statsLabel");
         statsLabel->setGeometry(QRect(703, 763, 514, 297));
         statsLabel->setFrameShape(QFrame::Shape::Box);
+        cpuLabel = new QLabel(statsLabel);
+        cpuLabel->setObjectName("cpuLabel");
+        cpuLabel->setGeometry(QRect(10, 0, 504, 45));
+        cpuLabel->setStyleSheet(QString::fromUtf8("\n"
+"       font-weight: 400;\n"
+"       font-size: 16pt;\n"
+"       text-transform: uppercase;\n"
+"      "));
+        cpuBar = new QProgressBar(statsLabel);
+        cpuBar->setObjectName("cpuBar");
+        cpuBar->setGeometry(QRect(10, 45, 494, 40));
+        cpuBar->setValue(0);
+        cpuBar->setMinimum(0);
+        cpuBar->setMaximum(100);
+        cpuBar->setTextVisible(false);
+        memLabel = new QLabel(statsLabel);
+        memLabel->setObjectName("memLabel");
+        memLabel->setGeometry(QRect(10, 99, 504, 45));
+        memLabel->setStyleSheet(QString::fromUtf8("\n"
+"       font-weight: 400;\n"
+"       font-size: 16pt;\n"
+"       text-transform: uppercase;\n"
+"      "));
+        memBar = new QProgressBar(statsLabel);
+        memBar->setObjectName("memBar");
+        memBar->setGeometry(QRect(10, 144, 494, 40));
+        memBar->setValue(0);
+        memBar->setMinimum(0);
+        memBar->setMaximum(100);
+        memBar->setTextVisible(false);
+        uptimeLabel = new QLabel(statsLabel);
+        uptimeLabel->setObjectName("uptimeLabel");
+        uptimeLabel->setGeometry(QRect(5, 198, 504, 45));
+        resultsTitle = new QLabel(DisplayWidget);
+        resultsTitle->setObjectName("resultsTitle");
+        resultsTitle->setGeometry(QRect(1267, 128, 419, 36));
+        resultsTitle->setFrameShape(QFrame::Shape::Box);
+        resultsTitle->setStyleSheet(QString::fromUtf8("\n"
+"     background-color: rgb(255, 255, 255);\n"
+"     color: rgb(0, 0, 0);\n"
+"     font-weight: 600;\n"
+"     text-transform: uppercase;\n"
+"    "));
         resultsLabel = new QLabel(DisplayWidget);
         resultsLabel->setObjectName("resultsLabel");
         resultsLabel->setGeometry(QRect(1267, 179, 633, 881));
@@ -96,10 +189,12 @@ public:
     {
         DisplayWidget->setWindowTitle(QCoreApplication::translate("DisplayWidget", "William Westwood - CSM2024", nullptr));
         logoLabel->setText(QCoreApplication::translate("DisplayWidget", "Hello :)", nullptr));
-        progressLabel->setText(QCoreApplication::translate("DisplayWidget", "Progress Prompts", nullptr));
+        progressLabel->setText(QCoreApplication::translate("DisplayWidget", "Harvesting emotional data for collective good\342\200\246 Individual sentiment is secondary.", nullptr));
         frameTitle->setText(QCoreApplication::translate("DisplayWidget", "Vision - Form Scanner Preview", nullptr));
+        faceTitle->setText(QCoreApplication::translate("DisplayWidget", "Vision - Facial Recognition", nullptr));
         faceLabel->setText(QCoreApplication::translate("DisplayWidget", "Face Camera", nullptr));
-        statsLabel->setText(QCoreApplication::translate("DisplayWidget", "System Stats", nullptr));
+        statsTitle->setText(QCoreApplication::translate("DisplayWidget", "Data - System Statistics", nullptr));
+        resultsTitle->setText(QCoreApplication::translate("DisplayWidget", "Data - Form Results", nullptr));
     } // retranslateUi
 
 };
